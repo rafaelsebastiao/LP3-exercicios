29b06c39b1748bb9675dc86ded811f9ea17ccf82
Sebastiao Rafael (CaP/ETS3) <seb7ca@bosch.com>
2026-03-03 14:41:30 -0300

    modify exercise

--- src/main/java/lista_exercicios_26_02_to_05_03/Ex10A.java
+++ src/main/java/lista_exercicios_26_02_to_05_03/Ex10A.java
@@ -1,11 +1,42 @@
 package lista_exercicios_26_02_to_05_03;
 
+import java.util.Scanner;
+
 public class Ex10A {
 
 
     public static void main(String[] args) {
         //     // 10) Gancho LPR3: sobrecarga + ArrayList
         //    //A) Crie sobrecarga de calcularArea: (lado) e (base,altura).
+
+
+        Scanner sc = new Scanner(System.in);
+
+        int opc;
+        double lado1, lado2;
+
+        System.out.println("1 - Quadrado");
+        System.out.println("2 - Retângulo");
+
+        System.out.print("Digite a opção desejada: ");
+        opc = sc.nextInt();
+
+        if(opc == 1){
+            System.out.print("Informe o valor do lado: ");
+            lado1 = sc.nextDouble();
+
+            System.out.printf("\n\nA área do quadrado %.2fmX%.2fm vale: %.2fm²", lado1, lado1, calcularArea(lado1));
+        }
+        else{
+            System.out.print("Informe o valor da base: ");
+            lado1 = sc.nextDouble();
+
+            System.out.print("Informe o valor da altura: ");
+            lado2 = sc.nextDouble();
+
+            System.out.printf("\n\nA área do retângulo %.2fmX%.2fm vale: %.2fm²", lado1, lado2, calcularArea(lado1, lado2));
+        }
+
 
     }
 
